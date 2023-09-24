7c6fe6aa2394ca1ccf0d4d6a6935789f57f05961
DESKTOP-QUEAO1U\tuann <tuan.nguyen2208nat@hcmut.edu.vn>
2023-09-24 15:06:15 +0700

    Init for EX6

--- SOURCE CODE/Core/Inc/main.h
+++ SOURCE CODE/Core/Inc/main.h
@@ -57,6 +57,30 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define chan1_Pin GPIO_PIN_1
+#define chan1_GPIO_Port GPIOA
+#define chan2_Pin GPIO_PIN_2
+#define chan2_GPIO_Port GPIOA
+#define chan3_Pin GPIO_PIN_3
+#define chan3_GPIO_Port GPIOA
+#define chan4_Pin GPIO_PIN_4
+#define chan4_GPIO_Port GPIOA
+#define chan5_Pin GPIO_PIN_5
+#define chan5_GPIO_Port GPIOA
+#define chan6_Pin GPIO_PIN_6
+#define chan6_GPIO_Port GPIOA
+#define chan7_Pin GPIO_PIN_7
+#define chan7_GPIO_Port GPIOA
+#define chan8_Pin GPIO_PIN_8
+#define chan8_GPIO_Port GPIOA
+#define chan9_Pin GPIO_PIN_9
+#define chan9_GPIO_Port GPIOA
+#define chan10_Pin GPIO_PIN_10
+#define chan10_GPIO_Port GPIOA
+#define chan11_Pin GPIO_PIN_11
+#define chan11_GPIO_Port GPIOA
+#define chan12_Pin GPIO_PIN_12
+#define chan12_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
--- SOURCE CODE/Core/Src/main.c
+++ SOURCE CODE/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,7 +84,36 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+  int i=1;
+
+    void den1()
+    {
+  	    HAL_GPIO_WritePin(chan1_GPIO_Port, chan1_Pin,RESET);
+  		HAL_GPIO_WritePin(chan2_GPIO_Port, chan2_Pin,SET);
+  		HAL_GPIO_WritePin(chan3_GPIO_Port, chan3_Pin,SET);
+  		HAL_GPIO_WritePin(chan4_GPIO_Port, chan4_Pin,SET);
+  		HAL_GPIO_WritePin(chan5_GPIO_Port, chan5_Pin,SET);
+  		HAL_GPIO_WritePin(chan6_GPIO_Port, chan6_Pin,SET);
+  		HAL_GPIO_WritePin(chan7_GPIO_Port, chan7_Pin,SET);
+  		HAL_GPIO_WritePin(chan8_GPIO_Port, chan8_Pin,SET);
+  		HAL_GPIO_WritePin(chan9_GPIO_Port, chan9_Pin,SET);
+  		HAL_GPIO_WritePin(chan10_GPIO_Port, chan10_Pin,SET);
+  		HAL_GPIO_WritePin(chan11_GPIO_Port,chan11_Pin,SET);
+  		HAL_GPIO_WritePin(chan12_GPIO_Port, chan12_Pin,SET);
+    }
+    void den2(){ HAL_GPIO_WritePin(chan2_GPIO_Port, chan2_Pin,RESET); }
+    void den3(){ HAL_GPIO_WritePin(chan3_GPIO_Port, chan3_Pin,RESET); }
+    void den4(){ HAL_GPIO_WritePin(chan4_GPIO_Port, chan4_Pin,RESET); }
+    void den5(){ HAL_GPIO_WritePin(chan5_GPIO_Port, chan5_Pin,RESET); }
+    void den6(){ HAL_GPIO_WritePin(chan6_GPIO_Port, chan6_Pin,RESET); }
+    void den7(){ HAL_GPIO_WritePin(chan7_GPIO_Port, chan7_Pin,RESET); }
+    void den8(){ HAL_GPIO_WritePin(chan8_GPIO_Port, chan8_Pin,RESET); }
+    void den9(){ HAL_GPIO_WritePin(chan9_GPIO_Port, chan9_Pin,RESET); }
+    void den10(){ HAL_GPIO_WritePin(chan10_GPIO_Port, chan10_Pin,RESET); }
+    void den11(){ HAL_GPIO_WritePin(chan11_GPIO_Port, chan11_Pin,RESET); }
+    void den12(){ HAL_GPIO_WritePin(chan12_GPIO_Port, chan12_Pin,RESET); }
 
   /* USER CODE END 2 */
 
@@ -92,7 +122,23 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  switch (i)
+	  {
+	  case 1 : den1();break;
+	  case 2 : den2();break;
+	  case 3 : den3();break;
+	  case 4 : den4();break;
+	  case 5 : den5();break;
+	  case 6 : den6();break;
+	  case 7 : den7();break;
+	  case 8 : den8();break;
+	  case 9 : den9();break;
+	  case 10: den10();break;
+	  case 11: den11();break;
+	  case 12: den12();i=0;break;
+	  }
+	  HAL_Delay(1000);
+	  i++;
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -132,6 +178,40 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, chan1_Pin|chan2_Pin|chan3_Pin|chan4_Pin
+                          |chan5_Pin|chan6_Pin|chan7_Pin|chan8_Pin
+                          |chan9_Pin|chan10_Pin|chan11_Pin|chan12_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : chan1_Pin chan2_Pin chan3_Pin chan4_Pin
+                           chan5_Pin chan6_Pin chan7_Pin chan8_Pin
+                           chan9_Pin chan10_Pin chan11_Pin chan12_Pin */
+  GPIO_InitStruct.Pin = chan1_Pin|chan2_Pin|chan3_Pin|chan4_Pin
+                          |chan5_Pin|chan6_Pin|chan7_Pin|chan8_Pin
+                          |chan9_Pin|chan10_Pin|chan11_Pin|chan12_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
